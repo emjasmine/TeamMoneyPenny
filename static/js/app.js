18cef4629810cdc581a698c932f2777a174f06f8
eglulu6 <eglulu6@gmail.com>
2021-04-24 18:51:30 -0400

    updating html and js calculator

--- static/js/app.js
+++ static/js/app.js
@@ -5,17 +5,6 @@ const path = "../static/js/sampledata.json"
 // quandl & polygon combo json
 var sampledata = d3.json(path)
 
-/************************** On page load **************************************/
-
-sampledata.then(function (sampledata) 
-{
-    console.log(sampledata[0].datatable.data[0]);
-    //Create ALL stocks table
-    allStocksTable(sampledata) 
-    barChart(X_tkrsymb, Y_divtimesqty)
-    gauge(remaining_amount, spend_amount, dollar_min)
-});
-
 /********** Declare variables for use in functions *******/
 
 // for barchart
@@ -31,7 +20,43 @@ var spend_amount = 50;
 // colors list rgb [ [low], [med], [high] ]
 var colors = [[45, 160, 227], [152, 74, 255], [251, 43, 255]]
 
-/************************* Function Create All data table *************************/
+/****************************************************************
+                        On page load
+****************************************************************/
+sampledata.then(function (sampledata) 
+{
+    // check .json results console.log(sampledata[0].datatable.data[0]);
+    //Create ALL stocks table
+    allStocksTable(sampledata) 
+    barChart(X_tkrsymb, Y_divtimesqty)
+    gauge(remaining_amount, spend_amount, dollar_min)
+});
+
+/****************************************************************
+                        User Input
+****************************************************************/
+
+// Identify user input field
+var dollars_input = d3.select('#spend-amount');
+
+// redefine variables for use in functions
+var spend_amount = parseFloat(dollars_input.property('value')).toFixed(2);
+console.log(spend_amount)
+
+// set remaining amount to spend amount for full gauge & inital filter value
+var remaining_amount = spend_amount;
+console.log(remaining_amount)
+
+// Identify button for change event
+var getStocks_button = d3.select('#get-stocks')
+
+// getStocks_button.on('click', getStocks_event(params))
+
+/****************************************************************
+                        FUNCTIONS
+****************************************************************/
+
+/************************* Create All data table *************************/
 function allStocksTable(data) 
 {
     // refrence the table body to build table in 
@@ -57,7 +82,7 @@ function allStocksTable(data)
     });
 }
 
-/************************* Function Bar Chart *************************/
+/************************* Bar Chart *************************/
 function barChart(x,y) 
 {
 //Create a blank chart 
@@ -73,9 +98,18 @@ var data =
 Plotly.newPlot('bar-chart', data);
 }
 
-/************************* Function gauge *************************/
+/************************* Gauge *************************/
 function gauge(remaining_amount, spend_amount, dollar_min)
 {
+    // check for zero dollar spend amount so that gauge doesnt error
+    if (parseInt(d3.select('#spend-amount').property('value')) === 0.00) 
+    {
+        spend_amount = 50
+    } 
+    // check values
+    console.log(`Remaining Amount ${remaining_amount}`)
+    console.log(`Spend Amount ${spend_amount}`)
+
     var data = 
     [{
         value: remaining_amount,
@@ -103,4 +137,72 @@ function gauge(remaining_amount, spend_amount, dollar_min)
     Plotly.newPlot('gauge', data, layout);
 }
 
-/************************* Function Filter Table *************************/
+/************************* Set input field always has 2 decimals *************************/
+function dollarsNotInt(event) {
+    let value = parseFloat(event.value);
+    if (Number.isNaN(value)) {
+      document.getElementById('spend-amount').value = "0.00";
+    } else {
+      document.getElementById('spend-amount').value = value.toFixed(2);
+    }              
+  }
+
+/************************* Filter Table *************************/
+function filterTable(data) 
+{
+    // refrence the table body to build table in 
+    var tbody = d3.select('.stock-table-body');
+    // Use for each to create a table with ALL the data
+    data[0].datatable.data.forEach(stock => 
+    {
+        //Append a tr to the table body tag
+        var row = tbody.append('tr');
+        // Append td.text  for each info column
+        // qty col does not currently contain input
+        row.append("td").text('');
+        // tkr symbol col
+        row.append("td").text(stock[1]);
+        // company name
+        row.append("td").text(stock[0]);
+        // stock price
+        row.append("td").text(stock[3]);
+        // dividend rate
+        row.append("td").text(stock[2]);
+        // rate of return
+        row.append("td").text(stock[4]);
+    });
+
+//     // If the filter function returns no tickers that have a price less than or equal to remaining amount then the length will be zero and we return an error msg:
+// if (filteredData.length === 0 ){
+//     // define the table header inorder to clear it
+//     var thead = d3.select('thead');
+//     // clear entire table
+//     tbody.text('');
+//     // create one row for error msg
+//     var row = tbody.append('tr');
+//     // append stats to the list
+//     row.append(‘td’).attr("colspan", “6”).text('You're out of money!');}
+//     Else
+//     // clear table of any previous data
+//     tbody.html("");
+//     //create a table with matched data the data	
+//     tableData.forEach(ticker => { 
+//     // append a row for each ticker	
+//     var row = tbody.append('tr').attr(“class”, “tkr_row”);
+//      // append columns	  		
+//     row.append("td").append(‘input’).attr(‘placeholder’, ‘Enter Qty’).classed(“input_col”); 
+//     row.append(“td”).classed(“tkr”).text(“ticker.ticker_name”)
+//     row.append(“td”).classed(“price”).text(“ticker.price”)
+//     row.append("td").text(ticker.other_info); }); //end for each    
+// }
+
+// /************************* getStocks_event *************************/
+
+// // redefine remaining amount
+
+// // Call gauge function
+// gauge(remaining_amount, spend_amount, dollar_min)
+
+// // Call table filter function
+// filterTable(data)
+}
