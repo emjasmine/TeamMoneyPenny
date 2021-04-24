3164c40ec1dd94e1300672c25d6c67bec01560bc
Emerald Desktop <eglulu6@gmail.com>
2021-04-23 23:24:54 -0400

    updating js file with begining code and html file with bones for table

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,45 @@
 /***************** Fetch data from JSON file *******************/
 //json path
-const path = "../data/stockexample.json"
+const path = "../data/sampledata.json"
 
-//quandl json for tiker data
-const quandlJson = d3.json(path);
-console.log(quandlJson);
-//polygon json for dividend data
-const polygonJson = d3.json(path);
-console.log(polygonJson);
-//combo json
-const stockexample = d3.json(path)
+// quandl & polygon combo json
+d3.json(path).then(function(data) {
+    console.log(data);
+  });
 
 /***************** On page load ********************************/
 //Create ALL stocks table
-stockexample.then(allStocksTable(stockexample));
+
 
 //Declare variables for barchart
-Var X_tkrsymb = [ ];
-Var Y_divtimesqty = [ ];
-
-
+var X_tkrsymb = [ ];
+var Y_divtimesqty = [ ];
 
 /************************* Function Create All data table *************************/
-function allStocksTable(data) 
-{
-    //Append a tr to the table body tag
+// function allStocksTable(data) 
+// {
+//     // refrence the table body to build table in 
+//     var tbody = d3.select('.stock-table-body');
+//     sampledata.datatable.data.forEach(stock => 
+//     {
+//         //Append a tr to the table body tag
+//         var row = tbody.append('tr');
+//         // qty col does not currently contain input
+//         row.append("td").text('');
+//         // tkr symbol col
+//         row.append("td").text(stock[1]);
+//         // company name
+//         row.append("td").text(stock[0]);
+//         // stock price
+//         row.append("td").text(stock[3]);
+//         // dividend rate
+//         row.append("td").text(stock[2]);
+//         // rate of return
+//         row.append("td").text(stock[4]);
+//     });
+
 
     //Use for each to create a table with ALL the data
 
     //Append td.text  for each info column
-}
+// }
